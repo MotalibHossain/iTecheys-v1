bc09ea5ac2aef07daa57e2fe1935aebb33886d7a
Motalib Hossain <52135729+MotalibHossain@users.noreply.github.com>
2022-09-21 00:04:41 +0600

    update
    update

---
+++ BackEnd/App_Article/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    # author=models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_author")
+    title=models.CharField(max_length=80)
+    slug=models.SlugField(max_length=80)
+    description=models.CharField(max_length=150)
+    # blog_image=models.ImageField(upload_to="Articles/", verbose_name="Image")
+    publish_date=models.DateTimeField(auto_now_add=True)
+    update_date=models.DateTimeField(auto_now=True)
+    # published=models.BooleanField(default=True)
+        
+    class Meta:
+        ordering = ['-publish_date']
+
+    def __str__(self) -> str:
+        return self.title
